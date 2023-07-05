cf285033954896aecc89a187f8e0e23c815e567f
axxt <axxt@example.com>
2023-07-05 18:31:08 +0800

    eplayer sendmsg 还有错

--- Language/c++/program/Eplayer/main.cpp
+++ Language/c++/program/Eplayer/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include<unistd.h>
 #include<functional>
+#include<memory.h>
+#include<sys/socket.h>  //socket_pair
+#include<sys/types.h>   //类型的定义
+#include<sys/stat.h>    //system的状态值
+#include<fcntl.h>       //文件操作
 class CFunctionBase{
 public:
     virtual ~CFunctionBase(){}
@@ -10,18 +15,29 @@ public:
 template<typename _FUNCTION_,typename... _ARGS_>
 class CFunction:public CFunctionBase{
 public:
-    CFunction(_FUNCTION_ func,_ARGS_... args){}
+    // 这里必须对m_binder进行初始化，否则会报错
+    //  error: no matching function for call to ‘std::_Bind_result<int, int (*(CProcess*))(CProcess*)>::_Bind_result()’
+    CFunction(_FUNCTION_ func,_ARGS_... args)
+        :m_binder(std::forward<_FUNCTION_>(func),std::forward<_ARGS_>(args)...)         //std::forward实现完美转发
+    {
+
+    }
     virtual ~CFunction(){}
     virtual int operator()(){
         return m_binder();
     }
-    std::_Bindres_helper<int, _FUNCTION_,_ARGS_...>::type m_binder;
+    // 这里必须加typename，告知编译器这里是一个类型。否则会报错
+    //error: need ‘typename’ before ‘std::_Bindres_helper<int, _FUNCTION_, _ARGS_ ...>::type’ because ‘std::_Bindres_helper<int, _FUNCTION_, _ARGS_ ...>’ is a dependent scope
+    // std::_Bindres_helper<int, _FUNCTION_,_ARGS_...>::type m_binder;
+    typename std::_Bindres_helper<int, _FUNCTION_,_ARGS_...>::type m_binder;
 };
+
 
 class CProcess{
 public:
     CProcess(){
         m_func = NULL;
+        memset(pipes,0,sizeof(pipes));
     }
     virtual ~CProcess(){
         //因为有new，所以需要析构函数来释放
@@ -33,20 +49,93 @@ public:
     }
     template<typename _FUNCTION_,typename... _ARGS_>        // 可变参数模板
     int SetEntryFunction(_FUNCTION_ func,_ARGS_... args){
-        m_func = new CFunction(func,args...);       //定义的时候在类型后加，使用的时候，在实参后加
+        m_func = new CFunction<_FUNCTION_,_ARGS_...>(func,args...);       //“...” 定义的时候在类型后加，使用的时候，在实参后加
         return 0;
     }
     int CreateSubProcess(){
         if(m_func == NULL) return -1;
-        pid_t pid = fork();
-        if (pid==-1) return -2;
+        int ret =socketpair(AF_LOCAL,SOCK_STREAM,0,pipes);
+        if(ret == -1) return -2;
+        pid_t pid = fork();         // 子进程复制了父进程的所有内容，包括这里的pipes，所以父进程有一个pipes[2],子进程也有一个pipes[2],这两个pipes通过socketpair函数连接在一起。
+        if (pid==-1) return -3;
 
         // 子进程
         if(pid == 0){
+            close(pipes[1]);        //关闭写端
+            pipes[1] = 0;
             return (*m_func)();
         }
+
         // 父进程
+
+        close(pipes[0]);            //关闭读端
+        pipes[0] = 0;
+
         m_pid = pid;
+        return 0;
+    }
+
+    // 主进程发送文件描述符
+    int SendFD(int fd){
+
+        // 这里的消息主体是不重要的
+        struct msghdr msg;
+        iovec iov[2];
+        char buf[][10] ={"qq","kk"};
+        iov[0].iov_base = buf[0];         // 最后我们传递的是文件描述符，所以这里的消息缓冲不重要，作为一种冗余放在这里。
+        iov[0].iov_len = sizeof(buf[0]);
+        iov[1].iov_base = buf[1];
+        iov[1].iov_len = sizeof(buf[1]);
+        msg.msg_iov= iov;
+        msg.msg_iovlen = 2;
+
+
+        // 下面的数据才是我们需要传递的数据，之前的都是幌子
+        cmsghdr * cmsg =  new (std::nothrow) cmsghdr;       //std::nothrow参数来告诉new操作符不要抛出异常，而是返回一个空指针。然后，我们检查指针p是否为空，以确定内存是否分配成功。
+        if(cmsg == nullptr) return -1;
+        bzero(cmsg,sizeof(cmsghdr));        //前两句可以简写为c的方式：cmsghdr * cmsg = (cmsghdr *) calloc(1, CMSG_LEN(sizeof(int)));如果采用这种，那么下面的delete需要修改为free
+        cmsg->cmsg_len = CMSG_LEN(sizeof(int));     // #define CMSG_LEN(len)   (CMSG_ALIGN (sizeof (struct cmsghdr)) + (len))
+        cmsg->cmsg_level = SOL_SOCKET;
+        cmsg->cmsg_type = SCM_RIGHTS;
+        *(int*) CMSG_DATA(cmsg) = fd;
+
+
+        msg.msg_control = cmsg;     // control msg
+        msg.msg_controllen = cmsg->cmsg_len;
+
+        ssize_t ret = sendmsg(pipes[1],&msg,0);
+        delete cmsg;
+        if(ret ==-1)return -2;
+
+        return 0;
+    }
+
+    int RecvFD(int & fd){
+        msghdr msg;
+        iovec iov[2];
+        char buf[][10] = {"",""};
+        iov[0].iov_base = buf[0];
+        iov[0].iov_len = sizeof(buf[0]);
+        iov[1].iov_base = buf[1];
+        iov[1].iov_len = sizeof(buf[1]);
+        msg.msg_iov = iov;
+        msg.msg_iovlen = 2;
+
+        cmsghdr * cmsg = (cmsghdr *) calloc(1, CMSG_LEN(sizeof(int)));
+        if (cmsg ==NULL) return -1;
+        cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+        cmsg->cmsg_level = SOL_SOCKET;
+        cmsg->cmsg_type = SCM_RIGHTS;
+
+        msg.msg_control = cmsg;
+        msg.msg_controllen = cmsg->cmsg_len;
+
+        ssize_t ret = recvmsg(pipes[0],&msg,0);
+        if (ret ==-1) {
+            free(cmsg);
+            return -2;
+        }
+        fd = *(int *) CMSG_DATA(cmsg);
         return 0;
     }
 private:
@@ -54,16 +143,38 @@ private:
     // 如果这里用了CFunction<T1,T2>, 由于CFunction是一个模板类，那么在声明其类型的时候肯定使用类型模板T1，这就会导致CProcess变成一个模板类。
     CFunctionBase* m_func;
     pid_t m_pid;
+    int pipes[2];       //0读，1写。子进程关闭写端，父进程关闭读端。
 };
 int CreaeLogServer(CProcess * proc){
+    printf("%s(%d):<%s> pid=%d\n", __FILE__, __LINE__, __FUNCTION__, getpid());
     return 0;
 }
 int CreaeClientServer(CProcess * proc){
+    printf("%s(%d):<%s> pid=%d\n", __FILE__, __LINE__, __FUNCTION__, getpid());
+    int fd = -1;
+    int ret = proc->RecvFD(fd);
+    printf("%s(%d):<%s> ret=%d\n", __FILE__, __LINE__, __FUNCTION__, ret);
+    printf("%s(%d):<%s> fd=%d\n", __FILE__, __LINE__, __FUNCTION__, fd);
+    sleep(1);
+    char buf[10] = "";
+    lseek(fd, 0, SEEK_SET);
+    read(fd, buf, sizeof(buf));
+    printf("%s(%d):<%s> buf=%s\n", __FILE__, __LINE__, __FUNCTION__, buf);
+    close(fd);
     return 0;
 }
 int main() {
     std::cout << "Hello, World!" << std::endl;
     CProcess proclog,procclient;
-//    proclog.set
+    proclog.SetEntryFunction(CreaeLogServer,&proclog);          // 可以自动推导类型
+    proclog.SetEntryFunction(CreaeClientServer,&procclient);
+    procclient.CreateSubProcess();
+    int fd = open("./test.txt",O_RDWR | O_CREAT | O_APPEND);
+    printf("errno:%d msg:%s\n", errno, strerror(errno));
+    if (fd ==-1) return -3;
+    write(fd,"qqqkkkhhh",9);
+    int ret =procclient.SendFD(fd);
+    if (ret != 0) printf("errno:%d msg:%s\n", errno, strerror(errno));
+    close(fd);
     return 0;
 }
